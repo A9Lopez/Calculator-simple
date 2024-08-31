28bd9dae9bd1b712cd03de379a6a32d192841a7c
Adrián López Ortiz <lopezortizadrian@gmail.com>
2024-08-31 20:32:11 +0200

    Definition of basic arithmetic functions created

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,21 @@
 module Main (main) where
-
 import Lib
 
+-- Definition of basic arithmetic functions
+
+add :: Float -> Float -> Float
+add x y = x + y
+
+subtract :: Float -> Float -> Float
+subtract x y = x - y
+
+multiply :: Float -> Float -> Float
+multiply x y = x * y
+
+divide :: Float -> Float -> Float
+divide x 0 = error "Cannot divide by 0"
+divide x y = x / y
+
+-- Main function to run the calculator
 main :: IO ()
 main = someFunc
