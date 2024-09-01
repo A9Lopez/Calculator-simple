f0c1ffa512cec46f798f139f93dbe2b3f21ad4d0
Adrián López Ortiz <lopezortizadrian@gmail.com>
2024-09-01 19:28:04 +0200

    basis calculator working

--- app/Main.hs
+++ app/Main.hs
@@ -1,25 +1,22 @@
 module Main (main) where
-import Lib
 
 -- Definition of basic arithmetic functions
-
 add :: Float -> Float -> Float
 add x y = x + y
 
-subtract :: Float -> Float -> Float
-subtract x y = x - y
+subtract' :: Float -> Float -> Float
+subtract' x y = x - y
 
 multiply :: Float -> Float -> Float
 multiply x y = x * y
 
 divide :: Float -> Float -> Float
-divide x 0 = error "Cannot divide by 0"
+divide _ 0 = error "Cannot divide by 0"
 divide x y = x / y
 
 -- Main function to run the calculator
 main :: IO ()
-main = someFunc
-
+main = do
     putStrLn "Welcome to the basic Haskell calculator"
     putStrLn "Choose an operation: (+, -, *, /)"
     operation <- getLine
@@ -32,7 +29,7 @@ main = someFunc
     let y = read num2 :: Float
     let result = case operation of
                     "+" -> add x y
-                    "-" -> subtract x y
+                    "-" -> subtract' x y
                     "*" -> multiply x y
                     "/" -> divide x y
                     _   -> error "Invalid operation"
